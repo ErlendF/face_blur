5fcd2ae0e776da54d9b25e24263cb56dac97f71a
Erlend Fonnes <erlend.fonnes@gmail.com>
2022-02-05 23:32:19 +0100

    fix: package restructuring

---
+++ blur/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup, find_packages
+
+setup(
+    name='Blur',
+    version='0.1',
+    description='Bluring utility',
+    author='Erlend Fonnes',
+    author_email='erlend.fonnes@gmail.com',
+    packages=find_packages(),
+    zip_safe=False,
+)
