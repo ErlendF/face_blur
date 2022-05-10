2f6171e3f8bc9766c0f95db7a2773b096be3159b
Erlend Fonnes <erlend.fonnes@gmail.com>
2022-05-10 11:05:54 +0200

    fix: added full_process to exported functions

--- videoface/__init__.py
+++ videoface/__init__.py
@@ -7,6 +7,7 @@ from .face_recognition import face_recognition_process
 from .deep_face import deep_face_process
 from .shot_transitions import get_shot_transitions
 from .filter_faces import init_known_faces, filter_known_faces, filter_selected_face
+from .full_processing import full_process
 
 __all__ = ["map_faces", "compare_faces", "interpolate", "face_recognition_process", "compare",
-           "dynamically_process", "make_sequences", "read_frame", "write_faces", "copy_remaining_files", "deep_face_process", "display_bboxes", "get_shot_transitions", "filter_known_faces", "init_known_faces", "filter_selected_face"]
+           "dynamically_process", "make_sequences", "read_frame", "write_faces", "copy_remaining_files", "deep_face_process", "display_bboxes", "get_shot_transitions", "filter_known_faces", "init_known_faces", "filter_selected_face", "full_process"]
