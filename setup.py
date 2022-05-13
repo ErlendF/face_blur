1d862727e2ce0c7e13eaeee0af6de00ebe4ef47e
Erlend Fonnes <erlend.fonnes@gmail.com>
2022-05-13 18:04:13 +0200

    fix: updating dependencies and package name

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ml_utils',
+    name='blur_face',
     version='0.1',
     description='ML utilities',
     author='Erlend Fonnes',
     packages=find_packages(),
-    install_requires=["numpy", "opencv-python", "matplotlib", "face_recognition", "ipython",
-                      "jupyter", "pandas", "sympy", "nose", "scipy", "deepface", "keras", "tensorflow>=2.1"]
+    install_requires=["numpy", "opencv-python", "matplotlib", "pandas",
+                      "sympy", "nose", "scipy", "deepface", "keras", "tensorflow>=2.1"]
 )
