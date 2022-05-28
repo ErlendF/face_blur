6d8dc0d8bd601dadb1d2cbf19dcab2aa464f0fba
Erlend Fonnes <erlend.fonnes@gmail.com>
2022-05-28 13:05:10 +0200

    fix: added reading all frames to exported functions

--- videoface/__init__.py
+++ videoface/__init__.py
@@ -2,7 +2,7 @@ from .dist import map_faces, compare_faces, compare
 from .interpolate import interpolate
 from .dynamic_processing import dynamically_process
 from .facial_sequences import make_sequences
-from .file import read_frame, write_faces, copy_remaining_files, display_bboxes
+from .file import read_frame, write_faces, copy_remaining_files, display_bboxes, read_all_frames
 from .dlib import dlib_process
 from .deep_face import deep_face_process
 from .shot_transitions import get_shot_transitions
@@ -10,4 +10,4 @@ from .filter_faces import init_known_faces, filter_known_faces, filter_selected_
 from .full_processing import full_process
 
 __all__ = ["map_faces", "compare_faces", "interpolate", "compare",
-           "dynamically_process", "make_sequences", "read_frame", "write_faces", "copy_remaining_files", "deep_face_process", "display_bboxes", "get_shot_transitions", "filter_known_faces", "init_known_faces", "filter_selected_face", "filter_short_sequences", "full_process", "dlib_process"]
+           "dynamically_process", "make_sequences", "read_frame", "write_faces", "copy_remaining_files", "deep_face_process", "display_bboxes", "get_shot_transitions", "filter_known_faces", "init_known_faces", "filter_selected_face", "filter_short_sequences", "full_process", "dlib_process", "read_all_frames"]
