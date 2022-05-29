b1aab5f507e579dbc4c49064721783d39af0a3c2
Erlend Fonnes <erlend.fonnes@gmail.com>
2022-05-29 22:55:51 +0200

    feat: added early version of s3 upload script

---
+++ scripts/s3/ex_variables.py
@@ -0,0 +1,2 @@
+region = "eu-north-1"
+aws_account = "my-aws-account"
---
+++ scripts/s3/s3_upload.py
@@ -0,0 +1,33 @@
+import boto3
+from videoface import get_shot_transitions
+from glob import glob
+from os import environ
+from sys import exit
+
+# Storing more sensitive variables in separate file to avoid commiting them
+import variables
+
+def upload_folder_by_shot(dir, dest):
+    environ["AWS_PROFILE"] = variables.aws_account
+    environ["AWS_DEFAULT_REGION"] = variables.region
+    s3_client = boto3.client('s3')
+
+    shots = get_shot_transitions(dir)
+    shots = [s for s in shots]
+
+    dest = dest.removesuffix("/")
+    current = 0
+
+    for i, filepath in enumerate(sorted(glob(dir))):
+        if i > shots[current]:
+            current += 1
+
+        filename = filepath.removeprefix(dir).removeprefix("/")
+        resp = s3_client.upload_file(filepath, dest + "/" + current + "/" + filename)
+        if resp["ResponseMetadata"]["HTTPStatusCode"] != 200:
+                print("Could not upload file to s3: {}".format(resp))
+                exit(1)
+
+
+
+
