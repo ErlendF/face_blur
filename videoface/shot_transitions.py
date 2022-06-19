c92689d8d342c8c9fe308483df120afb022205bf
Erlend Fonnes <erlend.fonnes@gmail.com>
2022-06-19 12:59:38 +0200

    feat: limiting shot transitions to the first and last frames, like processing

--- videoface/shot_transitions.py
+++ videoface/shot_transitions.py
@@ -4,17 +4,31 @@ from os.path import join
 import numpy as np
 from transnetv2 import TransNetV2
 
+from .file import get_file_name
 from .next_list import NextList
 
 model = TransNetV2()
 
 
-def get_shot_transitions(img_dir, file_ext="png", shot_transition_threshold=0.5, frames=None):
+def get_shot_transitions(img_dir, file_ext="png", shot_transition_threshold=0.5, frames=None, first_frame=None, last_frame=None):
     st = NextList()
     st_frames = []
+
     if frames is None:
-        for filepath in sorted(glob(join(img_dir, "*" + file_ext))):
-            st_frames.append(resize(imread(filepath), dsize=(48, 27)))
+        if first_frame is None:
+            first_frame = 0
+
+        if last_frame is None:
+            # Finding the last frame
+            last_frame = sorted(glob(join(img_dir, "*." + file_ext)))[-1]
+
+            # +1 for / and . in filename, -1 to make the frame numbers 0-indexed
+            last_frame = int(
+                last_frame[len(img_dir)+1+len("img"):len(last_frame)-(len(file_ext)+1)]) - 1
+
+        for img_nr in range(first_frame, last_frame+1):
+            st_frames.append(
+                resize(imread(get_file_name(img_nr, img_dir)), dsize=(48, 27)))
     else:
         for i in range(len(frames)):
             st_frames.append(resize(frames[i], dsize=(48, 27)))
